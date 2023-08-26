bb1464252a563d09273855ab9bdaa124ef0bb5eb
Govardhan <kurubagovardhan32@gmail.com>
2023-08-26 16:12:25 +0530

    code added

---
+++ src/main/java/com/driver/model/Repo.java
@@ -0,0 +1,132 @@
+package com.driver.model;
+
+import java.util.*;
+
+
+public class Repo {
+    HashMap<String,Hotel> hote = new HashMap<>();
+    HashMap<Integer,User> user = new HashMap<>();
+    HashMap<String,Booking> bookinghashMap = new HashMap<>();
+    public String addHotel(Hotel hot) {
+      /* if(hot.getHotelName() == null){
+            return "FAILURE";
+        }
+        if(hot == null) return "FAILURE";
+        for(Hotel ho : hotel.values()){
+            if(ho.getHotelName().equals(hot.getHotelName())){
+                return "FAILURE";
+            }
+        }
+        hotel.put(hot.getHotelName(),hot);
+        return "SUCCESS";*/
+        if (hot.getHotelName() == null)return "FAILURE";
+        if (hote.containsKey(hot.getHotelName()))return "FAILURE";
+        String hotelName = hot.getHotelName();
+        hote.put(hotelName, hot);
+        return "SUCCESS";
+    }
+
+    public Integer addUser(User us){
+        if(user.containsKey(us.getaadharCardNo())) return null;
+        user.put(us.getaadharCardNo(),us);
+        return us.getaadharCardNo();
+    }
+    public String getHotelWithMostFacilities(){
+        int max = 0;
+        for(Hotel h : hote.values()){
+            List<Facility> list = h.getFacilities();
+            max = Math.max(max,list.size());
+        }
+        List<String>al = new ArrayList<>();
+        if(max == 0) return "";
+        for(Hotel h : hote.values()){
+            List<Facility>list = h.getFacilities();
+            if(list.size() == max)
+                al.add(h.getHotelName());
+        }
+        Collections.sort(al);
+        return al.get(0);
+       /* int maxFacility = 0;
+        for (String key : hotelHashMap.keySet()) {
+            List<Facility> facilities = hotelHashMap.get(key).getFacilities();
+            maxFacility = Math.max(maxFacility, facilities.size());
+        }
+
+        if (maxFacility == 0) return "";
+        List<String> hotelNames = new ArrayList<>();
+        for (String key : hotelHashMap.keySet()) {
+            List<Facility> facilities = hotelHashMap.get(key).getFacilities();
+            if (facilities.size() == maxFacility) hotelNames.add(key);
+        }
+        Collections.sort(hotelNames);
+        return hotelNames.get(0);*/
+    }
+    public int bookARoom(Booking booking){
+        if(!hote.containsKey(booking.getHotelName())){
+            return -1;
+        }
+        Hotel h = hote.get(booking.getHotelName());
+        if(h.getAvailableRooms()>=booking.getNoOfRooms()){
+            int remainingrooms = h.getAvailableRooms()-booking.getNoOfRooms();
+            h.setAvailableRooms(remainingrooms);
+            String name = h.getHotelName();
+            hote.put(name,h);
+            String ss = UUID.randomUUID()+"";
+            int amount = booking.getNoOfRooms()*h.getPricePerNight();
+            booking.setBookingId(ss);
+            booking.setAmountToBePaid(amount);
+            bookinghashMap.put(ss,booking);
+            return amount;
+        }
+        return -1;
+
+    }
+    public int getBookings(int a){
+        int count = 0;
+        for(Booking b : bookinghashMap.values()){
+            if(b.getBookingAadharCard() == a){
+                count++;
+            }
+        }
+        return count;
+    }
+    public Hotel updateFacilities(List<Facility>newFacilities,String hotelName){
+       /* if(!hotel.containsKey(h)){
+            return null;
+        }
+        Hotel hot = hotel.get(h);
+        List<Facility>al = hot.getFacilities();
+        for(int i = 0; i<list.size();i++){
+            if(!al.contains(list.get(i))){
+                al.add(list.get(i));
+            }
+        }
+        hot.setFacilities(al);
+        // String s = hot.getHotelName();
+        hotel.put(h,hot);
+        return hot;
+    }*/
+        if(!hote.containsKey(hotelName))return null;
+
+        Hotel hotel = hote.get(hotelName);
+
+        List<Facility> list = hotel.getFacilities();
+
+        for(int i=0;i<newFacilities.size();i++){
+
+            if(!list.contains(newFacilities.get(i))){
+
+                list.add(newFacilities.get(i));
+            }
+
+        }
+
+        hotel.setFacilities(list);
+
+        hote.put(hotelName,hotel);
+
+        return  hotel;
+
+
+    }
+}
